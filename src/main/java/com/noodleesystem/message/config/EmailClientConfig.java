9be4278356003a93f84e8cd206f0262bf45a71cc
Robert Suszek <robert.suszek96@gmail.com>
2019-09-11 23:38:10 +0200

    Added Email Model Added javax.mail dependency Added EmailClientConfig Added EmailSenderWorker

--- src/main/java/com/noodleesystem/message/config/EmailClientConfig.java
+++ src/main/java/com/noodleesystem/message/config/EmailClientConfig.java
@@ -1,14 +1,50 @@
 package com.noodleesystem.message.config;
 
-public class EmailClientConfig {
-    private String host;
-    private String from;
-    private String password;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import java.util.Properties;
 
+public class EmailClientConfig {
+    private final String host;
+    private final String from;
+    private final String password;
+    private int port;
+    private boolean useAuth;
+    private boolean useStarttls;
+    private Properties properties;
+    private Authenticator authenticator;
 
     public EmailClientConfig(){
         host = "mail.thomas-it.pl";
         from = "projektpp@thomas-it.pl";
         password = "ppjestsuper1234";
+        port = 587;
+        useAuth = true;
+        useStarttls = true;
+
+        properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", useAuth);
+        properties.put("mail.smtp.starttls.enable", useStarttls);
+
+        authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        };
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public Authenticator getAuthenticator() {
+        return authenticator;
+    }
+
+    public String getFrom() {
+        return from;
     }
 }
--- src/main/java/com/noodleesystem/message/worker/EmailSenderWorker.java
+++ src/main/java/com/noodleesystem/message/worker/EmailSenderWorker.java
@@ -1,22 +1,70 @@
 package com.noodleesystem.message.worker;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.noodleesystem.message.config.EmailClientConfig;
+import com.noodleesystem.message.model.Email;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 @Component
 public class EmailSenderWorker {
     private ObjectMapper mapper;
-    private static EmailClientConfig config;
+    private EmailClientConfig config;
 
 
     public EmailSenderWorker() {
         this.mapper = new ObjectMapper();
-
+        config = new EmailClientConfig();
     }
 
     @RabbitListener(queues = "emails_queue")
-    public void sendEmail(String emailJsonString){
+    public void receive(String emailJsonString) {
+        try {
+            Email email = mapper.readValue(emailJsonString, Email.class);
+        } catch (JsonParseException e) {
+            System.err.println("JSON parsing error while trying to parse Email.");
+        } catch (JsonMappingException e) {
+            System.err.println("JSON mapping error while trying to parse Email.");
+        } catch (IOException e) {
+            System.err.println("IOException while trying to parse Email.");
+        }
+    }
+
+    private void send(Email email){
+        Session session = Session.getInstance(config.getProperties(), config.getAuthenticator());
+        prepareMimeAndSend(email, session);
+    }
+
+    private void prepareMimeAndSend(Email email, Session session){
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+            msg.setFrom(new InternetAddress(config.getFrom(), "NoReply-Noodlee"));
+            msg.setReplyTo(InternetAddress.parse(config.getFrom(), false));
+            msg.setSubject(email.getSubcjet());
+            msg.setText(email.getBody());
+            msg.setSentDate(new Date());
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getTo(), false));
+            Transport.send(msg);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
